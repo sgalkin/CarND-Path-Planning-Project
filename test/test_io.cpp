1d1a003d7e6ceb6c57ea844fe4c0bbf256cfa462
Sergey Galkin <sergey.v.galkin@yandex.ru>
2018-03-09 01:35:06 +0100

    negative cases for json parsing

--- test/test_io.cpp
+++ test/test_io.cpp
@@ -5,59 +5,103 @@
 #include "json.hpp"
 #include "io.h"
 
-namespace {
-  const std::string message =
-    "[\"telemetry\", {" 
-    "\"x\": 909.48,"
-    "\"y\": 1128.67,"
-    "\"yaw\": -90,"
-    "\"speed\": 30,"
-    "\"s\": 124.8336,"
-    "\"d\": 6.164833,"
-    "\"previous_path_x\": [10, 11],"
-    "\"previous_path_y\": [12, 21],"
-    "\"end_path_s\": 1044,"
-    "\"end_path_d\": -2,"
-    "\"sensor_fusion\": ["
-    "[ 0,815.2019,1128.931,54.22955,-0.5962787,30.73384,5.999703],"
-    "[ 3,775.8,1432.9,0,0,6713.911,-285.7268]"
-    "]"
-    "}]";
-}
-
 TEST_CASE("JsonParse") {
-  Json j;
-  auto m = j(message);
-  REQUIRE(m.ego.heading.x == Approx(909.48));
-  REQUIRE(m.ego.heading.y == Approx(1128.67));
-  REQUIRE(m.ego.heading.theta == Approx(-M_PI/2));
-  REQUIRE(m.ego.velocity == Approx(13.4112));
-  REQUIRE(m.ego.frenet.x == Approx(124.8336));
-  REQUIRE(m.ego.frenet.y == Approx(6.164833));
+  SECTION("Bad message. Header") {
+    const std::string message = "[\"info:\", 42]";
+    REQUIRE_THROWS(Json()(message));
+  }
+  SECTION("Bad message. Path mismatch") {
+    const std::string message =
+      "[\"telemetry\", {" 
+      "\"x\": 909.48,"
+      "\"y\": 1128.67,"
+      "\"yaw\": -90,"
+      "\"speed\": 30,"
+      "\"s\": 124.8336,"
+      "\"d\": 6.164833,"
+      "\"previous_path_x\": [10, 11],"
+      "\"previous_path_y\": [12],"
+      "\"end_path_s\": 1044,"
+      "\"end_path_d\": -2,"
+      "\"sensor_fusion\": ["
+      "[ 0,815.2019,1128.931,54.22955,-0.5962787,30.73384,5.999703],"
+      "[ 3,775.8,1432.9,0,0,6713.911,-285.7268]"
+      "]"
+      "}]";
+    REQUIRE_THROWS(Json()(message));
+  }
+  SECTION("Bad message. Invalid Fusion") {
+    const std::string message =
+      "[\"telemetry\", {" 
+      "\"x\": 909.48,"
+      "\"y\": 1128.67,"
+      "\"yaw\": -90,"
+      "\"speed\": 30,"
+      "\"s\": 124.8336,"
+      "\"d\": 6.164833,"
+      "\"previous_path_x\": [10, 11],"
+      "\"previous_path_y\": [12, 10],"
+      "\"end_path_s\": 1044,"
+      "\"end_path_d\": -2,"
+      "\"sensor_fusion\": ["
+      "[ 0,815.2019,1128.931,54.22955,-0.5962787,30.73384,5.999703,42],"
+      "[ 3,775.8,1432.9,0,6713.911,-285.7268]"
+      "]"
+      "}]";
+    REQUIRE_THROWS(Json()(message));
+  }
+  SECTION("Good message") {    
+    const std::string message =
+      "[\"telemetry\", {" 
+      "\"x\": 909.48,"
+      "\"y\": 1128.67,"
+      "\"yaw\": -90,"
+      "\"speed\": 30,"
+      "\"s\": 124.8336,"
+      "\"d\": 6.164833,"
+      "\"previous_path_x\": [10, 11],"
+      "\"previous_path_y\": [12, 21],"
+      "\"end_path_s\": 1044,"
+      "\"end_path_d\": -2,"
+      "\"sensor_fusion\": ["
+      "[ 0,815.2019,1128.931,54.22955,-0.5962787,30.73384,5.999703],"
+      "[ 3,775.8,1432.9,0,0,6713.911,-285.7268]"
+      "]"
+      "}]";
 
-  REQUIRE(m.path.size() == 2);
-  REQUIRE(m.path[0].x == Approx(10));
-  REQUIRE(m.path[0].y == Approx(12));
-  REQUIRE(m.path[1].x == Approx(11));
-  REQUIRE(m.path[1].y == Approx(21));
+    Json j;
+    auto m = j(message);
+    REQUIRE(m.ego.heading.x == Approx(909.48));
+    REQUIRE(m.ego.heading.y == Approx(1128.67));
+    REQUIRE(m.ego.heading.theta == Approx(-M_PI/2));
+    REQUIRE(m.ego.velocity == Approx(13.4112));
+    REQUIRE(m.ego.frenet.x == Approx(124.8336));
+    REQUIRE(m.ego.frenet.y == Approx(6.164833));
 
-  REQUIRE(m.destination.x == Approx(1044));
-  REQUIRE(m.destination.y == Approx(-2));
+    REQUIRE(m.path.size() == 2);
+    REQUIRE(m.path[0].x == Approx(10));
+    REQUIRE(m.path[0].y == Approx(12));
+    REQUIRE(m.path[1].x == Approx(11));
+    REQUIRE(m.path[1].y == Approx(21));
 
-  REQUIRE(m.fusion.size() == 2);
-  REQUIRE(m.fusion.at(0).position.x == Approx(815.2019));
-  REQUIRE(m.fusion.at(0).position.y == Approx(1128.931));
-  REQUIRE(m.fusion.at(0).velocity.x == Approx(54.22955));
-  REQUIRE(m.fusion.at(0).velocity.y == Approx(-0.5962787));
-  REQUIRE(m.fusion.at(0).frenet.x == Approx(30.73384));
-  REQUIRE(m.fusion.at(0).frenet.y == Approx(5.999703));
+    REQUIRE(m.destination.x == Approx(1044));
+    REQUIRE(m.destination.y == Approx(-2));
 
-  REQUIRE(m.fusion.at(3).position.x == Approx(775.8));
-  REQUIRE(m.fusion.at(3).position.y == Approx(1432.9));
-  REQUIRE(m.fusion.at(3).velocity.x == Approx(0));
-  REQUIRE(m.fusion.at(3).velocity.y == Approx(0));
-  REQUIRE(m.fusion.at(3).frenet.x == Approx(6713.911));
-  REQUIRE(m.fusion.at(3).frenet.y == Approx(-285.7268));
+    REQUIRE(m.fusion.size() == 2);
+    REQUIRE(m.fusion.at(0).position.x == Approx(815.2019));
+    REQUIRE(m.fusion.at(0).position.y == Approx(1128.931));
+    REQUIRE(m.fusion.at(0).velocity.x == Approx(54.22955));
+    REQUIRE(m.fusion.at(0).velocity.y == Approx(-0.5962787));
+    REQUIRE(m.fusion.at(0).frenet.x == Approx(30.73384));
+    REQUIRE(m.fusion.at(0).frenet.y == Approx(5.999703));
+
+    REQUIRE(m.fusion.at(3).position.x == Approx(775.8));
+    REQUIRE(m.fusion.at(3).position.y == Approx(1432.9));
+    REQUIRE(m.fusion.at(3).velocity.x == Approx(0));
+    REQUIRE(m.fusion.at(3).velocity.y == Approx(0));
+    REQUIRE(m.fusion.at(3).frenet.x == Approx(6713.911));
+    REQUIRE(m.fusion.at(3).frenet.y == Approx(-285.7268));
+  }
 }
 
 TEST_CASE("JsonCompose") {
